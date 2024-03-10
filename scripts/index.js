8f4529d05b02a1e42d00e3da58a778516214be38
Oleg Zozulya <yumaoff@yandex.ru>
2024-03-10 22:38:21 +0300

    fix deleteFunc

--- scripts/index.js
+++ scripts/index.js
@@ -10,18 +10,22 @@
 
 const cardList = document.querySelector('.places__list');
 
-function showCard(name, link) {
+function deleteCard (element) {
+  element.remove();
+}
+
+function showCard(name, link, deleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   cardElement.querySelector('.card__image').src = link;
   cardElement.querySelector('.card__title').textContent = name;
   cardList.append(cardElement);
-  cardDeleteButton.addEventListener('click', function (){
-    cardElement.remove();
+  cardDeleteButton.addEventListener('click', function(){
+    deleteCard(cardElement);
   });
 };
 
 for (i = 0; i < initialCards.length; i++) {
-  showCard(initialCards[i].name,initialCards[i].link);
+  showCard(initialCards[i].name,initialCards[i].link, deleteCard);
 };

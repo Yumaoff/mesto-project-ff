dfe00605c0a450b2a828ebdad19357c2c8ac6025
Oleg Zozulya <yumaoff@yandex.ru>
2024-03-11 00:16:53 +0300

    fix2

--- scripts/index.js
+++ scripts/index.js
@@ -14,18 +14,27 @@ function deleteCard (element) {
   element.remove();
 }
 
-function showCard(name, link, deleteCard) {
+function createCard(card, deleteCard) {
   const cardTemplate = document.querySelector('#card-template').content;
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__title').textContent = name;
-  cardList.append(cardElement);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle =  cardElement.querySelector('.card__title');
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
   cardDeleteButton.addEventListener('click', function(){
     deleteCard(cardElement);
   });
+  return cardElement;
+};
+
+
+function showCard(card, deleteCard) {
+  const cardElement = createCard(card, deleteCard);
+  cardList.append(cardElement);
 };
 
 for (i = 0; i < initialCards.length; i++) {
-  showCard(initialCards[i].name,initialCards[i].link, deleteCard);
+  showCard(initialCards[i], deleteCard);
 };

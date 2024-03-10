9cdf1660f20d6c374155150f716e6ba1b71d68a5
Oleg Zozulya <yumaoff@yandex.ru>
2024-03-10 18:12:50 +0300

    Обновление карточек и их удаление по нажатию

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,21 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+
+function showCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardList.append(cardElement);
+  cardDeleteButton.addEventListener('click', function (){
+    cardElement.remove();
+  });
+};
+
+for (i = 0; i < initialCards.length; i++) {
+  showCard(initialCards[i].name,initialCards[i].link);
+};

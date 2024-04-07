9aba9994630919f0e09d981ce5bfd08e92f81003
Oleg Zozulya <yumaoff@yandex.ru>
2024-04-07 17:59:15 +0300

    Добавлена сборка webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,4 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+import '../pages/index.css';
 
 const cardList = document.querySelector('.places__list');
 
